1abf443a38bff77299e4dc9872911e5ef8cf5f51
Tarun Ranka <tarunranka.m@gmail.com>
2025-03-08 13:51:45 +0400

    added environment variable and used in dashboard page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -77,9 +77,9 @@ const Navbar = () => {
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 class="lucide lucide-box h-8 w-8 text-blue-600">
                 <path d="M21 8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16Z"></path>
                 <path d="m3.3 7 8.7 5 8.7-5"></path>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,3 +1,12 @@
+const API_BASE_URL =
+  import.meta.env.VITE_API_ENDPOINT || "http://localhost:5173/api";
+
+const SALES_API_URL = `${API_BASE_URL}/sales`;
+const STOCK_API_URL = `${API_BASE_URL}/inventory`;
+
+console.log("Sales API:", SALES_API_URL);
+console.log("Stock API:", STOCK_API_URL);
+
 export default function Dashboard() {
   return <h2 className="text-xl font-semibold mb-4">Dashboard</h2>;
 }
